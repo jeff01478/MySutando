0124c01b259b182e9ab432747af9a762a5874051
2400843P <johnjiang@fstop.com.tw>
2026-03-17 11:58:17 +0800

    feat: 新增 MessageEvent，觸發新訊息事件發送

---
+++ src/main/java/com/john/mysutando/event/MessageEvent.java
@@ -0,0 +1,41 @@
+package com.john.mysutando.event;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import com.john.mysutando.service.DiscordTextService;
+
+import jakarta.annotation.Nonnull;
+import lombok.RequiredArgsConstructor;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
+
+@Component
+@RequiredArgsConstructor
+public class MessageEvent extends DiscordEvent {
+    private static final Logger log = LogManager.getLogger(MessageEvent.class);
+
+    private final DiscordTextService discordTextService;
+
+    @Override
+    public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
+        log.info("start onMessageReceived");
+        if (event.getAuthor().isBot() || !event.isFromType(ChannelType.TEXT)) {
+            return;
+        }
+        Message message = event.getMessage();
+        discordTextService.recordMessage(message);
+    }
+
+    @Override
+    public void onMessageUpdate(@Nonnull MessageUpdateEvent event) {
+    }
+
+    @Override
+    public void onMessageDelete(@Nonnull MessageDeleteEvent event) {
+    }
+}
